556bf1d61175615f4f1bae09ada1b35a79113e83
Brandard Emuria <brandardemuria47@gmail.com>
2020-09-30 16:51:32 +0100

    Final Commit
    My Final Unity Game Roll Ball

--- Brandard/Assets/Scripts/PlayerController.cs
+++ Brandard/Assets/Scripts/PlayerController.cs
@@ -2,23 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
-
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+    
+    
+    private int count;
     private Rigidbody rb;
-
     private float movementX;
     private float movementY;
 
+
     // Start is called before the first frame update
     void Start()
-    {
+    { 
         rb = GetComponent<Rigidbody>();
+
+        count = 0;
+
+        SetCountText();
+
+        winTextObject.SetActive(false);
+
     }
 
-     void OnMove(InputValue movementValue)
+
+    void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
 
@@ -26,11 +39,36 @@ public class PlayerController : MonoBehaviour
         movementY = movementVector.y;
     }
 
-     void FixedUpdate()
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+        if (count >= 11)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
+
+
+    void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
 
         rb.AddForce(movement * speed);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+
+            count = count + 1;
+
+            SetCountText();
+        }
+        
     }
 
 }
